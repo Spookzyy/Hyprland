557e4c2fdd7dc4601c40b94a19650c893bbc43ce
Jacob Harvey-Hale <84041117+Spookzyy@users.noreply.github.com>
2025-02-17 22:22:31 +0000

    Update and rename install.sh to installfirefox.sh

--- config/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-clear
-
-yay
-
-sudo pacman -S steam goverlay git btop waybar discord vesktop "hyprpaper YAY COMMAND"
---
+++ config/installfirefox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+clear
+
+yay
+
+sudo pacman -Sy firefox
+
+firefox
